474bf08c8d6e4d914d07cde5e19a11a7860bca01
Naveen-9-9 <bunnynaveen363@gmail.com>
2026-05-18 09:15:00 +0530

    feat: initialize next.js web application

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@workspace/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'me7aitdbxq.ufs.sh',
+      },
+    ],
+  },
+}
+
+export default nextConfig
